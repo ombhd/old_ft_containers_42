c942dcefbfe4702bec77ba91a16e0b9ede0cd1ff
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-04-25 16:52:13 +0000

    save

--- list.hpp
+++ list.hpp
@@ -11,20 +11,45 @@
 /* ************************************************************************** */
 
 #ifndef LIST_HPP
-# define LIST_HPP
+#define LIST_HPP
+
+#include "./node.hpp"
+
+#include <vector>
+#include <list>
+
+std::vector<int> vec;
+std::list<int> lst;
 
 namespace ft
 {
-	template  < class T >
+	template <class T>
 	class list
 	{
-		private:
-			
-		public:
-			list(){}
-			list(const list &);
-			list &operator=(const list &);
-			~list();
+	private:
+		Node<T> *_head;
+		Node<T> *_tail;
+		size_t _len;
+
+	public:
+		list<T>() : _len(0)
+		{
+			_head = _tail = new Node<T>();
+		}
+
+		list<T>(size_t n, const T &val = static_cast<T>(0))
+		{
+			_len = n;
+			for (size_t i = 0; i < _len; i++)
+			{
+			}
+		}
+
+		list<T>(const list<T> &) {}
+
+		list<T> &operator=(const list<T> &) {}
+
+		~list<T>() {}
 	};
 } // namespace ft
 #endif // !LIST_HPP
--- node.hpp
+++ node.hpp
@@ -6,16 +6,54 @@
 /*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/21 16:02:42 by obouykou          #+#    #+#             */
-/*   Updated: 2021/04/21 16:03:48 by obouykou         ###   ########.fr       */
+/*   Updated: 2021/04/24 12:43:43 by obouykou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include <iostream>
+
 namespace ft
 {
+	template <class T>
+	class Node
+	{
+	public:
+		T data;
+		Node<T> *next;
+		Node<T> *prev;
 
+		Node<T>() : next(NULL), prev(NULL) {}
+
+		Node<T>(T inData) : data(inData), next(NULL), prev(NULL) {}
+
+		Node<T>(Node<T> const &src)
+		{
+			*this = src;
+		}
+
+		Node<T> &operator=(Node<T> const &src)
+		{
+			if (this != &src)
+			{
+				this->data = src.data;
+				this->next = src.next;
+				this->prev = src.prev;
+			}
+			return *this;
+		}
+
+		virtual ~Node<T>();
+
+		void	push(Node<T> *nd)
+		{
+			this->next = nd;
+			if (nd)
+				nd->prev = this;
+		}
+	};
 } // namespace ft
 
 #endif // !NODE_HPP
